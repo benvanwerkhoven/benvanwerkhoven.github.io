06df6a6d7bb9aad38c179ff14a892ce30a423a1a
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2017-07-01 23:00:51 +0200

    fix the creative theme

--- js/creative.js
+++ js/creative.js
@@ -42,6 +42,23 @@
         }
     })
 
+    // Initialize and Configure Magnific Popup Lightbox Plugin
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
+
+
     // Initialize WOW.js Scrolling Animations
     new WOW().init();
 
